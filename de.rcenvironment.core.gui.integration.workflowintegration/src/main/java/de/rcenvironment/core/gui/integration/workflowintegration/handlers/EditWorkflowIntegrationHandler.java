dbb95c84335454d78472e6f54f8aa6910740ea43
Kathrin Schaffert <kathrin.schaffert@dlr.de>
2023-05-09 13:27:42 +0200

    MANTIS-ID 18019: deactivated context menu for remote or renamed/removed local workflow components

--- de.rcenvironment.core.gui.integration.workflowintegration/src/main/java/de/rcenvironment/core/gui/integration/workflowintegration/handlers/EditWorkflowIntegrationHandler.java
+++ de.rcenvironment.core.gui.integration.workflowintegration/src/main/java/de/rcenvironment/core/gui/integration/workflowintegration/handlers/EditWorkflowIntegrationHandler.java
@@ -57,10 +57,10 @@ public class EditWorkflowIntegrationHandler extends AbstractHandler {
             workflowIntegrationController = new WorkflowIntegrationController(integrationName);
         } catch (FileNotFoundException e) {
 
-            MessageDialog.openError(activeWorkbenchWindow.getShell(), "Workflow Integration Editor", StringUtils.format(
-                "The Workflow Integration Editor cannot be displayed. \nThe following file does not exist: \"%s\" "
-                    + "\nThe selected component may have been renamed or removed from local drive.",
-                e.getMessage()));
+            // Should never happen -> TODO: remove Dialog
+            MessageDialog.openError(activeWorkbenchWindow.getShell(), "Workflow Integration Editor",
+                "The Workflow Integration Editor cannot be displayed."
+                    + "\nEither the selected component is a remote component, or it may have been renamed or removed from local drive.");
 
             LOG.error(StringUtils.format(
                 "The Workflow Integration Editor cannot be displayed. The following file does not exist: \"%s\" ",
