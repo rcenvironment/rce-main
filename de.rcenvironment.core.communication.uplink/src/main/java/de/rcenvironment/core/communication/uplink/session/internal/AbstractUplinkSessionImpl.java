ffb81504bfb30a5f2ab58850964e8759348e9fe8
Mischke, Robert <robert.mischke@dlr.de>
2025-12-09 08:27:24 +0100

    MANTIS-18267: Tweaked Uplink state handling on connection breakdown; should fix rare "Unexpected transition" warnings

--- de.rcenvironment.core.communication.uplink/src/main/java/de/rcenvironment/core/communication/uplink/session/internal/AbstractUplinkSessionImpl.java
+++ de.rcenvironment.core.communication.uplink/src/main/java/de/rcenvironment/core/communication/uplink/session/internal/AbstractUplinkSessionImpl.java
@@ -218,14 +218,20 @@ public abstract class AbstractUplinkSessionImpl implements UplinkSession {
 
         public synchronized void markIncomingStreamClosedOrEOF() {
             if (incomingStreamClosedOrEOF) {
-                throw new IllegalStateException("Redundant call");
+                log.warn(logPrefix + "Redundant call to mark the incoming stream as closed or EOF");
+                return;
             }
             incomingStreamClosedOrEOF = true;
 
             if (mainState == UplinkSessionState.UNCLEAN_SHUTDOWN_INITIATED) {
-                log.debug(logPrefix
-                    + "Incoming stream closed after closing the local end of the stream; considering unclean shutdown complete");
-                setMainStateInternal(UplinkSessionState.UNCLEAN_SHUTDOWN);
+                if (outgoingStreamClosed) {
+                    log.debug(logPrefix
+                        + "Incoming stream closed after closing the local end of the stream; considering unclean shutdown complete");
+                    setMainStateInternal(UplinkSessionState.UNCLEAN_SHUTDOWN);
+                } else {
+                    log.debug(logPrefix + "Incoming stream marked as closed or EOF, "
+                        + "but the outgoing stream was not closed yet; postponing final state change");
+                }
                 return;
             }
 
